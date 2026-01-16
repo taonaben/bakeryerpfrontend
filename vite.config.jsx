fdf0711705d561c75c2abe1d44cf8ad1bddb3b85
Tafadzwa Zvokuomba <107610484+Tafadzwa92@users.noreply.github.com>
2026-01-16 13:50:00 +0200

    vercel changes

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    open: true
-  },
-
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/
-  }
+  // Ensure there is NO 'base' property here, 
+  // or if there is, it should be './' or '/'
 })
